1a54ba5fb2789b02db14f75d1bb81744b44e60ed
richard <theirsaviorhd@gmail.com>
2025-10-28 17:37:49 -0400

    CTA Section Component - cta section extends the front page php via template part - styles change dynamically to for great UX across all devices

--- wp-theme/front-page.php
+++ wp-theme/front-page.php
@@ -8,12 +8,9 @@
     <?php get_template_part('template-parts/features-section'); ?>
 
     <!-- CTA Section -->
-    <section class="cta" id="cta">
-        <div class="container">
-            <h2>Ready to take action?</h2>
-            <a href="#" class="btn-primary">Sign Up Now</a>
-        </div>
-    </section>
+    <?php get_template_part('template-parts/cta-section'); ?>
+
+
 </main>
 
 <?php get_footer(); ?>
---
+++ wp-theme/template-parts/cta-section.php
@@ -0,0 +1,12 @@
+    <section class="cta" id="cta">
+        <div class="cta-container">
+            <div class="cta-image">
+                 <i class="fa-solid fa-sailboat"></i>
+            </div>
+            <div class="cta-content">
+                <h2>Ready to take action?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, beatae. Eius vitae ad molestiae ex cupiditate doloremque, error praesentium reiciendis alias expedita nam illo quibusdam at dolorem itaque quidem rem?</p>
+                <a href="./" class="button-primary">Sign Up Now</a>
+            </div>
+        </div>
+    </section>
